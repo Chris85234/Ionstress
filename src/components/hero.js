b3f2d1500eaa629f20da1dfd119ecb64fa3744da
Astrid Orellana <astrid.orellaan2024@hilasal.superate.org.sv>
2025-05-19 14:59:18 -0600

    Cambio de background

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,8 +9,9 @@ const Hero = () => {
       <div className="hero-content">
         <h1 className="hero-title">Drones that detect the power of the planet</h1>
         <p className="hero-description">
-          We specialize in aerial inspections to improve the efficiency and sustainability of renewable energy systems.
+          We specialize in aerial inspections to improve the efficiency and sustainability of renewable energy systems
         </p>
+        <h5 className="hero-extra">In countries with high renewable energy potential like El Salvador, our drone inspections help reduce the carbon footprint by up to 20% annually per system, maximizing energy efficiency and eliminating the use of polluting transportation.</h5>
         <button className="cta-button">Get inspection</button>
       </div>
     </section>
